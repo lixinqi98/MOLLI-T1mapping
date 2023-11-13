f9f61e5abaf550e0e40380af7cd7cdb920c2daa0
mona <lixinqi98@gmail.com>
2023-11-13 06:05:15 -0800

    scripts to run

---
+++ jobs_sh/run_t1.sh
@@ -0,0 +1,7 @@
+python T1map/MOCO_T1_stage1_PH.py --input data/patient_Liting/015 --output data/patient_Liting_dDCE_Mona/015
+
+python T1map/MOCO_T1_stage1_PH.py --input data/patient_Liting/017 --output data/patient_Liting_dDCE_Mona/017
+
+python T1map/MOCO_T1_stage1_PH.py --input data/patient_Liting/019 --output data/patient_Liting_dDCE_Mona/019
+
+python T1map/MOCO_T1_stage1_PH.py --input data/patient_Liting/075 --output data/patient_Liting_dDCE_Mona/075
